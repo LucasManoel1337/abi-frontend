57a6cfc17500a828d1af1edb3e15018beae16359
kayoGc <kayovictorgoncaloc@hotmail.com>
2025-05-22 15:25:30 -0300

    Não permitindo usuário acessar abas proibidas

--- src/js/utilidades/verificarLogin.js
+++ src/js/utilidades/verificarLogin.js
@@ -1,24 +1,34 @@
+import { GUIA, SOBRE, INDEX } from "../urls";
+
+const urlPermitidos = [GUIA, SOBRE, INDEX];
+
 export const verificar = () => {
 
     // pega o id do usuário no localstorage
     let user = localStorage.getItem('idUsuario');
-
-    // pega os botões de logar / deslogar
-    let botaoLogout = document.getElementById('btnLogout');
-    let botaoLogin = document.getElementById('btnLogin');
-
+    
+    
     // se o usuário estiver logado
     if (user) {
+        // pega os botões de logar / deslogar
+        const botaoLogout = document.getElementById('btnLogout');
+        const botaoLogin = document.getElementById('btnLogin');
         // ativa o de deslogar e desativa o de logar
         botaoLogout.style.display = 'block';
         botaoLogin.style.display = 'none';
         return;
     }
     
-    // se o usuário não estiver logado oculta alguns links
-    let linksParaDesativar = document.querySelector('nav').children;
-    let num = 2;
-    for (let i = 2, n = linksParaDesativar.length; i < n; i++) {
-        linksParaDesativar[num].remove();
-    }    
+    // se o usuário estiver em uma pagina permitida
+    if (urlPermitidos.includes(window.location)) {
+        // se o usuário não estiver logado oculta alguns links
+        let linksParaDesativar = document.querySelector('nav').children;
+        let num = 2;
+        for (let i = 2, n = linksParaDesativar.length; i < n; i++) {
+            linksParaDesativar[num].remove();
+        }    
+    } else {
+        window.location.replace(INDEX);
+    }
+    
 }
