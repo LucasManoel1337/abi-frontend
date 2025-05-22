e44d45291d224311b59297e075633ee3683cf11e
kayoGc <kayovictorgoncaloc@hotmail.com>
2025-05-22 15:42:26 -0300

    autalizando urls

--- src/js/urls.js
+++ src/js/urls.js
@@ -1,9 +1,9 @@
 // pagina de login
 export const LOGIN = 'https://abi-frontend-mu.vercel.app/src/paginas/Login.html'
 // pagina sobre o projeto
-export const SOBRE = 'https://abi-frontend-mu.vercel.app/src/paginas/Login.html'
+export const SOBRE = 'https://abi-frontend-mu.vercel.app/src/paginas/SobreProjeto.html'
 // pagina guia
-export const GUIA = 'https://abi-frontend-mu.vercel.app/src/paginas/Login.html'
+export const GUIA = 'https://abi-frontend-mu.vercel.app/src/paginas/Guia.html'
 // pagina principal
 export const INDEX = 'https://abi-frontend-mu.vercel.app/index.html'
 
